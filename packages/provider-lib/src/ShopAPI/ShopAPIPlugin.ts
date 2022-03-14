f68c039fff28443c7c9b1c9989d968661e931fe0
Josh Thomas <jthoms1@gmail.com>
2022-03-14 13:36:28 -0500

    make shop api use web for android and ios as well.

--- packages/provider-lib/src/ShopAPI/ShopAPIPlugin.ts
+++ packages/provider-lib/src/ShopAPI/ShopAPIPlugin.ts
@@ -3,6 +3,8 @@ import { ShopAPIPlugin } from './definitions';
 
 const ShopAPI = registerPlugin<ShopAPIPlugin>('ShopAPI', {
   web: () => import('./ShopAPIPluginWeb').then((m) => new m.ShopAPIPluginWeb()),
+  android: () => import('./ShopAPIPluginWeb').then((m) => new m.ShopAPIPluginWeb()),
+  ios: () => import('./ShopAPIPluginWeb').then((m) => new m.ShopAPIPluginWeb()),
 });
 
 export default ShopAPI;
